75f5caaf2e425bd126551accbc9d89b8e0791bc7
kgh458 <kgh458@mail.missouri.edu>
2016-09-14 19:28:21 -0500

    wrote psuedocode for ids and dfs...

---
+++ DFGS.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package homework2.ai;
+
+/**
+ *
+ * @author sugarhryszko
+ */
+public class DFGS {
+    private int row;
+    private int column;
+    
+    public DFGS(int row, int column){
+        this.row = row;
+        this.column = column;
+    }
+    
+    /*
+    checks closest if not visited, left&right before up&down 
+    keeps track of parent, and T/F value for visited
+    travels back to parent and checks other nodes until finds true
+    
+    
+    */
+}
---
+++ IDS.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package homework2.ai;
+
+/**
+ *
+ * @author Kara
+ */
+public class IDS {
+    private int row;
+    private int column;
+    
+    public IDS(int row, int column){
+        this.row = row;
+        this.column = column;
+    }
+    /*
+    set depth value (width + 1)
+    search through unvisited nodes only up to depth value
+    
+    
+    
+    */
+    
+}
